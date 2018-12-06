af3e8a110aa425fbccaec1c88699fe76c34d1033
Ed <red@redde-Mac-mini.local>
2018-12-06 11:56:50 +0800

    增加註釋

--- rbac/method.go
+++ rbac/method.go
@@ -9,8 +9,6 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-var url string
-
 type (
 	//conf && rule base
 	config struct {
@@ -19,18 +17,12 @@ type (
 	}
 )
 
-// SetURL --
-// mongodb://account:pass@localhost:27017/db
-//default db name "casbin"
-func SetURL(u string) {
-	url = u
-	return
-}
-
 // NewConfig --
 // mongodb://account:pass@localhost:27017/db
 // default db name "casbin"
 func NewConfig(u string) *config {
+	//預設conf ,rbac_with_services_model.conf, 可直接覆蓋更換
+	//預設mongo adapter , 可直接覆蓋更換
 	a := mongodbadapter.NewAdapter(u)
 	return &config{
 		CONF:    "./rbac_with_services_model.conf",
@@ -39,36 +31,43 @@ func NewConfig(u string) *config {
 }
 
 //check permission
+//輸入參數依照conf設定
+//這裡預設為 身份 , services name , api path , method
 func (c *config) Enforce(rvals ...interface{}) (b bool, err error) {
+	//instance cabin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//比對是否有相符資料存在
 	b, err = e.EnforceSafe(rvals...)
 	return
 }
 
 //add policy
+//輸入參數依照conf設定
+//這裡預設為 身份 , services name , api path , method
 func (c *config) AddPolicy(rvals ...interface{}) (b bool, err error) {
+	//instance cabin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//add policy
 	b, err = e.AddPolicySafe(rvals...)
+	//如果加入失敗和err nil時 , 檢查資料是否存在
 	if !b && err == nil {
 		if g := e.HasPolicy(rvals...); g {
 			err = errors.New("already exists")
@@ -78,36 +77,41 @@ func (c *config) AddPolicy(rvals ...interface{}) (b bool, err error) {
 }
 
 //remove policy
+//輸入參數依照conf設定
+//這裡預設為 身份 , services name , api path , method
 func (c *config) RemovePolicy(rvals ...interface{}) (b bool, err error) {
+	//instance cabin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//remove policy
 	b, err = e.RemovePolicySafe(rvals...)
 	return
 }
 
 //Add Group Policy
 func (c *config) AddGroupPolicy(rvals ...interface{}) (b bool, err error) {
+	//instance casbin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//add user to group
 	b = e.AddGroupingPolicy(rvals...)
+	//添加失敗,檢查是否已存在
 	if !b {
 		if g := e.HasGroupingPolicy(rvals...); g {
 			err = errors.New("already exists")
@@ -117,20 +121,21 @@ func (c *config) AddGroupPolicy(rvals ...interface{}) (b bool, err error) {
 	return
 }
 
-//Remove Group Plicy
+//Remove Group Plicy , 將user 從group 移除
 //Not removed return false
 func (c *config) RemoveGroupPolicy(rvals ...interface{}) (b bool, err error) {
+	//instance casbin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//remove group policy
 	b = e.RemoveGroupingPolicy(rvals...)
 
 	return
@@ -138,17 +143,18 @@ func (c *config) RemoveGroupPolicy(rvals ...interface{}) (b bool, err error) {
 
 //Policy exist return true
 func (c *config) HasPolicy(rvals ...interface{}) (b bool, err error) {
+	//instance casbin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	// 檢查是否已存在
 	b = e.HasPolicy(rvals...)
 
 	return
@@ -156,17 +162,18 @@ func (c *config) HasPolicy(rvals ...interface{}) (b bool, err error) {
 
 //Group Policy exist return true
 func (c *config) HasGroupPolicy(rvals ...interface{}) (b bool, err error) {
+	//instance casbin
 	e, err := casbin.NewEnforcerSafe(c.CONF, c.Adapter)
 	if err != nil {
 		return
 	}
-
+	//filter 身份
 	filter := &bson.M{"v0": rvals[0]}
 	e.LoadFilteredPolicy(filter)
 	if err != nil {
 		return
 	}
-
+	//檢查user 是否存在該group
 	b = e.HasGroupingPolicy(rvals...)
 
 	return
